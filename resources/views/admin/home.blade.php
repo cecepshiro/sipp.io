3ff46bb0d14ac98456d7e99e2d92f4cdd1e6e18f
Cecep Saefudin <saefudincecep@gmail.com>
2018-07-17 10:08:05 +0700

    tambah layout menu

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-                <div class="top-left-part"><a class="logo" href="{{ route('personil.index') }}"><b><img src="{{ asset('asset/image/logo.png') }}" alt="home" /></b><span class="hidden-xs"><img src="{{ asset('asset/image/dispsiad.png') }}" alt="home" /></span></a></div>
+                <div class="top-left-part"><a class="logo" href="{{ url('/') }}"><b><img src="{{ asset('asset/image/logo.png') }}" alt="home" /></b><span class="hidden-xs"><img src="{{ asset('asset/image/dispsiad.png') }}" alt="home" /></span></a></div>
                 <ul class="nav navbar-top-links navbar-left pull-left hidden-xs">
                     <li>
                     </li>
@@ -15,7 +15,7 @@
                    
                     <!-- /.dropdown -->
                     <li class="dropdown">
-                        <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="{{ asset('asset/plugins/images/users/varun.jpg') }}" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">{{ Auth::user()->name }}</b> </a>
+                        <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="{{ asset('image/avatar.png') }}" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">{{ Auth::user()->name }}</b> </a>
                         <ul class="dropdown-menu dropdown-user animated flipInY">                           
                             <form action="{{ action('PersonilController@profil') }}" method="get" enctype="multipart/form-data">
                                 {{ csrf_field() }}
@@ -33,12 +33,7 @@
                         <!-- /.dropdown-user -->
                     </li>
                     <!-- .Megamenu -->
-                    <li class="mega-dropdown"> <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"><span class="hidden-xs"></span> <i class="icon-options-vertical"></i></a>
-                        <ul class="dropdown-menu mega-dropdown-menu animated bounceInDown">
-                            
-                        </ul>
-                    </li>
-                    <!-- /.Megamenu -->
+                   
                    
                 </ul>
             </div>
@@ -59,19 +54,34 @@
                         <!-- /input-group -->
                     </li>
                     <li class="user-pro">
-                        <a href="#" class="waves-effect"><img src="{{ asset('asset/plugins/images/users/varun.jpg') }}" alt="user-img" class="img-circle"> <span class="hide-menu"> {{ Auth::user()->name }}<span class="fa arrow"></span></span>
+                        <a href="#" class="waves-effect"><img src="{{ asset('image/avatar.png') }}" alt="user-img" class="img-circle"> <span class="hide-menu"> {{ Auth::user()->name }}<span class="fa arrow"></span></span>
                         </a>
                         <ul class="nav nav-second-level">
-                            <li><a href=""><i class="ti-user"></i>Profil Diri</a></li>
-                            <li><a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();"><i class="fa fa-power-off"></i> Keluar</a></li>
+                            <li><form action="{{ action('PersonilController@profil') }}" method="get" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="id" value="{{ Auth::user()->id }}"><a></a>
+                                    <input type="hidden" name="profil_id" value="{{ Auth::user()->id_anggota }}"><a></a>
+                                    <a><button type="submit" style="float: left" class="btnCustom btnCustom-default"><i class="ti-user"></i> Profil Diri</button></a>
+                                </form>
+                            </li>
+                            <li><form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <a><button type="submit" style="float: left" class="btnCustom btnCustom-default"><i class="fa fa-power-off"></i> Keluar</button></a>
+                                </form>
+                            </li>
                         </ul>
                     </li>
                             <li class="nav-small-cap m-t-10">--- Data Personel</li>
-                            <li> <a href="{{ route('personil.index') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">List Personel</span></a>
+                            <li> <a href="{{ route('personil.index') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Daftar Personel</span></a>
+                            <li class="nav-small-cap m-t-10">--- Data Pengalaman</li>
+                            <li> <a href="{{ route('personil.index') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Daftar Pengalaman</span></a>
                             <li class="nav-small-cap m-t-10">--- Data Registrasi</li>
                             <li> <a href="{{ route('register') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Registrasi Pengguna</span></a>
+                            <li> <a href="{{ route('register') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Daftar Pengguna</span></a>
+                            <li class="nav-small-cap m-t-10">--- Data Master</li>
+                            <li> <a href="{{ route('agama.index') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Data Agama</span></a>
+                            <li class="nav-small-cap m-t-10">--- Data Laporan</li>
+                            <li> <a href="{{ route('agama.index') }}" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Laporan</span></a>
                 </ul>
             </div>
         </div>
@@ -196,7 +206,5 @@
                         </div>
                     </div>
                 </div>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
+                
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,87 +32,6 @@
                         </ul>
                         <!-- /.dropdown-user -->
                     </li>
-                    <!-- .Megamenu -->
-                    <li class="mega-dropdown"> <a class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"><span class="hidden-xs"></span> <i class="icon-options-vertical"></i></a>
-                        <ul class="dropdown-menu mega-dropdown-menu animated bounceInDown">
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li class="dropdown-header">Forms Elements</li>
-                                    <li><a href="form-basic.html">Basic Forms</a></li>
-                                    <li><a href="form-layout.html">Form Layout</a></li>
-                                    <li><a href="form-advanced.html">Form Addons</a></li>
-                                    <li><a href="form-material-elements.html">Form Material</a></li>
-                                    <li><a href="form-float-input.html">Form Float Input</a></li>
-                                    <li><a href="form-upload.html">File Upload</a></li>
-                                    <li><a href="form-mask.html">Form Mask</a></li>
-                                    <li><a href="form-img-cropper.html">Image Cropping</a></li>
-                                    <li><a href="form-validation.html">Form Validation</a></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li class="dropdown-header">Advance Forms</li>
-                                    <li><a href="form-dropzone.html">File Dropzone</a></li>
-                                    <li><a href="form-pickers.html">Form-pickers</a></li>
-                                    <li><a href="form-wizard.html">Form-wizards</a></li>
-                                    <li><a href="form-typehead.html">Typehead</a></li>
-                                    <li><a href="form-xeditable.html">X-editable</a></li>
-                                    <li><a href="form-summernote.html">Summernote</a></li>
-                                    <li><a href="form-bootstrap-wysihtml5.html">Bootstrap wysihtml5</a></li>
-                                    <li><a href="form-tinymce-wysihtml5.html">Tinymce wysihtml5</a></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li class="dropdown-header">Table Example</li>
-                                    <li><a href="basic-table.html">Basic Tables</a></li>
-                                    <li><a href="table-layouts.html">Table Layouts</a></li>
-                                    <li><a href="data-table.html">Data Table</a></li>
-                                    <li><a href="bootstrap-tables.html">Bootstrap Tables</a></li>
-                                    <li><a href="responsive-tables.html">Responsive Tables</a></li>
-                                    <li><a href="editable-tables.html">Editable Tables</a></li>
-                                    <li><a href="foo-tables.html">FooTables</a></li>
-                                    <li><a href="jsgrid.html">JsGrid Tables</a></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li class="dropdown-header">Charts</li>
-                                    <li> <a href="flot.html">Flot Charts</a> </li>
-                                    <li><a href="morris-chart.html">Morris Chart</a></li>
-                                    <li><a href="chart-js.html">Chart-js</a></li>
-                                    <li><a href="peity-chart.html">Peity Charts</a></li>
-                                    <li><a href="knob-chart.html">Knob Charts</a></li>
-                                    <li><a href="sparkline-chart.html">Sparkline charts</a></li>
-                                    <li><a href="extra-charts.html">Extra Charts</a></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-12 m-t-40 demo-box">
-                                <div class="row">
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-purple"><a href="http://wrappixel.com/demos/admin-templates/pixeladmin/inverse/index.html" target="_blank" class="text-white"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i><br/>Demo 1</a></div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-success"><a href="index.html" target="_blank" class="text-white"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i><br/>Demo 2</a></div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-info"><a href="https://wrappixel.com/demos/admin-templates/pixeladmin/dark/index.html" target="_blank" class="text-white"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i><br/>Demo 3</a></div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-inverse"><a href="https://wrappixel.com/demos/admin-templates/pixeladmin/horizontal-nav-fullwidth/index.html" target="_blank" class="text-white"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i><br/>Demo 4</a></div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-warning"><a href="https://wrappixel.com/demos/admin-templates/pixeladmin/inverse-rtl/index.html" target="_blank" class="text-white"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i><br/>Demo 5</a></div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="white-box text-center bg-danger"><a href="http://wrappixel.com/templates/pixeladmin/" target="_blank" class="text-white"><i class="linea-icon linea-ecommerce fa-fw" data-icon="d"></i><br/>Buy Now</a></div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- /.Megamenu -->
-                   
                 </ul>
             </div>
             <!-- /.navbar-header -->
@@ -184,7 +103,7 @@
                                                     <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nama Lengkap') }}</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                                        <input id="name" type="text" class="form-control form-control-line{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                                         @if ($errors->has('name'))
                                                             <span class="invalid-feedback" role="alert">
@@ -198,7 +117,7 @@
                                                     <label for="id_anggota" class="col-md-4 col-form-label text-md-right">{{ __('ID Anggota') }}</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="id_anggota" type="text" class="form-control{{ $errors->has('id_anggota') ? ' is-invalid' : '' }}" name="id_anggota" value="{{ old('id_anggota') }}" required>
+                                                        <input id="id_anggota" type="text" class="form-control form-control-line{{ $errors->has('id_anggota') ? ' is-invalid' : '' }}" name="id_anggota" value="{{ old('id_anggota') }}" required>
 
                                                         @if ($errors->has('id_anggota'))
                                                             <span class="invalid-feedback" role="alert">
@@ -212,7 +131,7 @@
                                                     <label for="akses" class="col-md-4 col-form-label text-md-right">{{ __('Hak Akses') }}</label>
 
                                                     <div class="col-md-6">
-                                                        <select id="akses" class="form-control{{ $errors->has('akses') ? ' is-invalid' : '' }}" name="akses" required autofocus>
+                                                        <select id="akses" class="form-control form-control-line{{ $errors->has('akses') ? ' is-invalid' : '' }}" name="akses" required autofocus>
                                                             <option>Pilih Hak Akses</option>
                                                             <option value="0">Admin</option>
                                                             <option value="1">Atasan</option>
@@ -230,7 +149,7 @@
                                                     <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                                        <input id="password" type="password" class="form-control form-control-line{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                                         @if ($errors->has('password'))
                                                             <span class="invalid-feedback" role="alert">
@@ -244,7 +163,7 @@
                                                     <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Konfirmasi Password') }}</label>
 
                                                     <div class="col-md-6">
-                                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                                        <input id="password-confirm" type="password" class="form-control form-control-line" name="password_confirmation" required>
                                                     </div>
                                                 </div>
 
